d36871e0bb6aa1b259530f55be8a8b8f94280eff
BeesanGhaith <beesan.ghaith@gmail.com>
2022-02-02 21:36:03 +0200

    update RoleRouter

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const roleRouter = express.Router();
+
+const { createNewRole } = require("../controllers/role");
+
+roleRouter.post("/", createNewRole);
+
 module.exports = roleRouter;
