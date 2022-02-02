ac364c93f6c6c3cf14cd0e2ca1bf160bc0de9b7a
Rubaalnadi <rubaalnadi5@gmail.com>
2022-02-02 16:16:23 +0200

    add functions of user in router user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
+const { createNewUser,getAllUser,getUserById } = require("../controllers/users");
+userRouter.post("/",createNewUser);
+userRouter.get("/",getAllUser);
+userRouter.get("/:id",getUserById);
 module.exports = userRouter;
