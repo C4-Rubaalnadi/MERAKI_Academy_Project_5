eeaf61d882afc877f501214c9ec3f13f946ebefd
BeesanGhaith <beesan.ghaith@gmail.com>
2022-02-02 20:08:56 +0200

    createNewRole

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,30 +1,22 @@
+const connection = require("../db/db");
 
+// This function creates new role
+const createNewRole = (req, res) => {
+  const role = req.body.role;
+  const query = `INSERT INTO roles (role) VALUES (?)`;
+  const data = [role];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: `Server Error` });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: `Success role created`,
+        role: result,
+        data: data
+      });
+    }
+  });
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports = {
-
-//   };
+module.exports = { createNewRole };
