7e91e616a672e91ae047133774482822a40ecbe6
Rubaalnadi <rubaalnadi5@gmail.com>
2022-02-02 16:13:58 +0200

    creat permission routes in server

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+
 const db = require("./database/db");
 //=======================================
 const usersRouter = require("./routes/users"); //import users router
 const loginRouter = require("./routes/login"); //import login router
 const roleRouter = require("./routes/role"); //import role router
 const productRouter = require("./routes/products"); //import products router
-
+const permissionRouter = require("./routes/permission"); //import permission router
+//========================================
+app.use(express.json());
+app.use(cors());
 //========================================
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/roles", roleRouter);
 app.use("/products", productRouter);
-
+app.use("/permission",permissionRouter);
 //=========================================
 
-app.use(cors());
 
-app.use(express.json());
 
 const PORT = 5000;
 
