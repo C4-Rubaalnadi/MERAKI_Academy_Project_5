81377f88b275ba1c49c1fbe1470edf1bdb71c275
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-02-07 01:51:39 +0200

    add icons to admin page

--- frontend/src/components/admin/admin.js
+++ frontend/src/components/admin/admin.js
@@ -1,22 +1,40 @@
 import React from "react";
 import Chart from "./chart";
-import {GrDashboard} from "react-icons/gr"
+import { GrDashboard } from "react-icons/gr";
+import { CgProductHunt } from "react-icons/cg";
+import { FiUsers } from "react-icons/fi";
+import { IoCartSharp } from "react-icons/io5";
 const Admin = () => {
   return (
     <div>
       <div className="adminPageContainer">
         <div className="adminPageWrapper">
-          
           <div className="pageContainer">
             <div className="sideBar">
-                <div className="adminDashboard"><GrDashboard/><button className="dashboardButton">Dasboard</button></div>
+              <div className="adminDashboard">
+                <GrDashboard />
+                <button className="dashboardButton">Dasboard</button>
+              </div>
+              <div className="adminDashboard">
+                <CgProductHunt />
+                <button className="dashboardButton">Products</button>
+              </div>
+              <div className="adminDashboard">
+                <FiUsers />
+                <button className="dashboardButton">Users</button>
+              </div>
+              <div className="adminDashboard">
+                <IoCartSharp />
+                <button className="dashboardButton">Orders</button>
+              </div>
             </div>
-            <div className="midPage"><Chart /></div>
+            <div className="midPage">
+              <Chart />
+            </div>
             <div className="leftPage"></div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
