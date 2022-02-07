a8ba906b9766a217bb461d5597f947cd27b8905d
BeesanGhaith <beesan.ghaith@gmail.com>
2022-02-08 00:14:15 +0200

    updateCartById

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cartRouter = express.Router();
-const { createCart,getAllCartOfUser,getAllOrder } = require("../controllers/cart");
+const { createCart,getAllCartOfUser,getAllOrder, updateCartById } = require("../controllers/cart");
 const authentication = require("../middleware/authentication")
 cartRouter.post("/",authentication ,createCart);
 cartRouter.get("/:id",getAllCartOfUser);
 cartRouter.get("/search",getAllOrder);
+cartRouter.put("/update/:id/:product_id", updateCartById)
 module.exports = cartRouter;
