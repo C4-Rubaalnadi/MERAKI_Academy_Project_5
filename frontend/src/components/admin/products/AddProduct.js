7b5e88e05851c81be5f4329c8966c5cbb9fb303a
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-02-10 00:21:41 +0200

    addProduct page

---
+++ frontend/src/components/admin/products/AddProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Cloudinary from "../../cloudinary/cloudinary";
+function AddProduct() {
+  return (
+    <div>
+      <Cloudinary />
+    </div>
+  );
+}
+
+export default AddProduct;
