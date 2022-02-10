e58b280508de2cf24103cbd0e831773b6a3ebec9
Rubaalnadi <rubaalnadi5@gmail.com>
2022-02-11 01:42:06 +0200

    modify cart rout

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -9,6 +9,8 @@ const {
   getAllOrder,
   updateCartById,
   deleteCartById,
+  Add_wishList,
+  getAllFavortListOfUser
 } = require("../controllers/cart");
 const authentication = require("../middleware/authentication");
 
@@ -17,5 +19,6 @@ cartRouter.get("/:id", getAllCartOfUser);
 cartRouter.get("/search/cart", getAllOrder);
 cartRouter.put("/edit/:id/", updateCartById);
 cartRouter.delete("/delete/:id/:product_id", deleteCartById);
-
+cartRouter.post("/add_wishList",Add_wishList);
+cartRouter.get("/FavortList/:idUser",getAllFavortListOfUser)
 module.exports = cartRouter;
